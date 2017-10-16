3cf3f0d17ad6f4efbfc2b87315b8d0f7829df205
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-10-16 15:44:15 +0100

    Add login from different browser page

--- packages/blockchain-wallet-v4-frontend/src/scenes/LoginAuthorization/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/LoginAuthorization/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actions } from 'data'
 
 import LoginAuthorization from './template.js'
 
 class LoginAuthorizationContainer extends React.Component {
   render () {
+    const { status } = this.props
     return (
-      <LoginAuthorization status='success' />
+      <LoginAuthorization status={status}
+        requestingBrowser='Chrome'
+        requestingIP='IP address'
+        requestingCountry='UK'
+        thisDeviceBrowser='Chrome'
+        thisDeviceIP='IP address'
+        thisDeviceCountry='UK' />
     )
   }
 }
 
-export default LoginAuthorizationContainer
+const mapStateToProps = (state) => ({
+  status: 'different browser'
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  alertActions: bindActionCreators(actions.alerts, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginAuthorizationContainer)
--- packages/blockchain-wallet-v4-frontend/src/scenes/LoginAuthorization/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/LoginAuthorization/template.js
@@ -3,7 +3,7 @@ import { Redirect } from 'react-router-dom'
 import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
 
-import { Icon, Separator, Text } from 'blockchain-info-components'
+import { Button, Icon, Separator, Text, TextGroup } from 'blockchain-info-components'
 
 const Wrapper = styled.div`
   width: 100%;
@@ -11,7 +11,9 @@ const Wrapper = styled.div`
   box-sizing: border-box;
   background-color: ${props => props.theme['white']};
 
-  @media(min-width: 768px) { width: 550px; }
+  @media(min-width: 768px) { 
+    width: 550px; 
+  }
 `
 const Header = styled.div`
   display: flex;
@@ -30,8 +32,26 @@ const SuccessIconWrapper = styled.div`
 const SuccessIcon = styled(Icon)`
   transform: scale(1.5, 1.5);
 `
-const SuccessText = styled(Text)`
+const HeaderText = styled(Text)`
   padding: 10px 0px;
+`
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`
+
+const ButtonContainer = styled(InfoContainer)`
+  display: flex;
+  justify-content: space-around;
+  padding-top: 10px;
 `
 
 const LoginAuthorization = (props) => {
@@ -44,14 +64,103 @@ const LoginAuthorization = (props) => {
             <SuccessIconWrapper>
               <SuccessIcon name='checkmark' color='success' />
             </SuccessIconWrapper>
-            <SuccessText size='18px' weight={300} capitalize>
+            <HeaderText size='18px' weight={300} capitalize>
               <FormattedMessage id='scenes.loginauthorization.success' defaultMessage='Success!' />
-            </SuccessText>
+            </HeaderText>
           </Header>
           <Text size='13px' weight={300}>
             <FormattedMessage id='scenes.loginauthorization.explain' defaultMessage='Login approved! Please return to your previous browser / tab to see your wallet.' />
           </Text>
           <Separator />
+        </Wrapper>
+      )
+
+    case 'different browser':
+      const { requestingBrowser, requestingIP, requestingCountry, thisDeviceBrowser, thisDeviceIP, thisDeviceCountry } = props
+      return (
+        <Wrapper>
+          <Header>
+            <HeaderText size='18px' weight={300} capitalize>
+              <FormattedMessage id='scenes.loginauthorization.otherbrowser' defaultMessage='Login attempt from other browser' />
+            </HeaderText>
+          </Header>
+          <TextGroup>
+            <Text size='13px' weight={300}>
+              <FormattedMessage id='scenes.loginauthorization.info' defaultMessage='Someone, hopefully you, is attempting to login to your wallet from a different browser.' />
+            </Text>
+            <Text size='13px' weight={300}>
+              <FormattedMessage id='scenes.loginauthorization.browserchanged' defaultMessage='Browser User Agent Changed' />
+            </Text>
+          </TextGroup>
+          <Separator />
+          <InfoContainer>
+            <InfoColumn>
+              <Text>
+                <FormattedMessage id='scenes.loginauthorization.requesting.title' defaultMessage='Requesting device' />
+              </Text>
+              <TextGroup inline>
+                <Text size='13px'>
+                  <FormattedMessage id='scenes.loginauthorization.requesting.browser' defaultMessage='Browser:' />
+                </Text>
+                <Text size='13px' weight={300}>
+                  <FormattedMessage id='scenes.loginauthorization.requesting.browserinfo' defaultMessage={requestingBrowser} />
+                </Text>
+              </TextGroup>
+              <TextGroup inline>
+                <Text size='13px'>
+                  <FormattedMessage id='scenes.loginauthorization.requesting.ip' defaultMessage='IP address:' />
+                </Text>
+                <Text size='13px' weight={300}>
+                  <FormattedMessage id='scenes.loginauthorization.requesting.ipinfo' defaultMessage={requestingIP} />
+                </Text>
+              </TextGroup>
+              <TextGroup inline>
+                <Text size='13px'>
+                  <FormattedMessage id='scenes.loginauthorization.requesting.country' defaultMessage='Country of Origin:' />
+                </Text>
+                <Text size='13px' weight={300}>
+                  <FormattedMessage id='scenes.loginauthorization.requesting.countryinfo' defaultMessage={requestingCountry} />
+                </Text>
+              </TextGroup>
+            </InfoColumn>
+            <InfoColumn>
+              <Text>
+                <FormattedMessage id='scenes.loginauthorization.thisdevice.title' defaultMessage='This device' />
+              </Text>
+              <TextGroup inline>
+                <Text size='13px'>
+                  <FormattedMessage id='scenes.loginauthorization.thisdevice.browser' defaultMessage='Browser:' />
+                </Text>
+                <Text size='13px' weight={300}>
+                  <FormattedMessage id='scenes.loginauthorization.thisdevice.browserinfo' defaultMessage={thisDeviceBrowser} />
+                </Text>
+              </TextGroup>
+              <TextGroup inline>
+                <Text size='13px'>
+                  <FormattedMessage id='scenes.loginauthorization.thisdevice.ip' defaultMessage='IP address:' />
+                </Text>
+                <Text size='13px' weight={300}>
+                  <FormattedMessage id='scenes.loginauthorization.thisdevice.ipinfo' defaultMessage={thisDeviceIP} />
+                </Text>
+              </TextGroup>
+              <TextGroup inline>
+                <Text size='13px'>
+                  <FormattedMessage id='scenes.loginauthorization.thisdevice.country' defaultMessage='Country of Origin:' />
+                </Text>
+                <Text size='13px' weight={300}>
+                  <FormattedMessage id='scenes.loginauthorization.thisdevice.countryinfo' defaultMessage={thisDeviceCountry} />
+                </Text>
+              </TextGroup>
+            </InfoColumn>
+          </InfoContainer>
+          <ButtonContainer>
+            <Button nature='primary'>
+              <FormattedMessage id='scenes.loginauthorization.authorize' defaultMessage='Authorize' />
+            </Button>
+            <Button nature='logout'>
+              <FormattedMessage id='scenes.loginauthorization.decline' defaultMessage='Decline' />
+            </Button>
+          </ButtonContainer>
         </Wrapper>
       )
     default:

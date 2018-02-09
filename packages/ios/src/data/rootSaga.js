1bc91cd51641b5c8da487568ca9213e9f9557d15
Maurice Achtenhagen <maurice@blockchain.com>
2018-02-09 10:47:09 -0500

    Console log loaded environment

--- packages/ios/src/data/rootSaga.js
+++ packages/ios/src/data/rootSaga.js
@@ -1,12 +1,13 @@
 // -- EXPOSE THE ROOT SAGA -- //
 import { all, call, fork } from 'redux-saga/effects'
 import login from './auth/sagas'
-
+import Config from 'react-native-config'
 const welcomeSaga = function * () {
   if (console) {
     const version = '4.0.0.0'
     const style = 'background: #16B5E7; color: #FFF; font-size: 14px;'
     console.log(`%c Blockchain iOS Wallet ${version} `, style)
+    console.log("Loaded Configuration", Config)
   }
 }
 
